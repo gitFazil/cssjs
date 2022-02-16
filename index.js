144aaeea084c3d6fe3c4e75eaa8b410a2c7c0d7d
Mohd Fazil <kfazil185@gmail.com>
2022-02-16 10:11:23 +0530

    add sort method

--- index.js
+++ index.js
@@ -1,23 +1,63 @@
-let a = "fazil";
-let b = a.split("").reverse().join("");
-console.log(b);
+// let a = "fazil";
+// let b = a.split("").reverse().join("");
+// console.log(b);
 
 
-var arr = ["this", "is", "my", "wish", "to", "work", "on"];
-let where = arr.length;
+// var arr = ["this", "is", "my", "wish", "to", "work", "on"];
+// let where = arr.length;
 
-arr.splice(where, 0, "website");
+// arr.splice(where, 0, "website");
 
-let wish = arr.slice(where);
+// let wish = arr.slice(where);
 
-console.log(arr.toString());
-console.log(wish.toString())
+// console.log(arr.toString());
+// console.log(wish.toString())
+let second = [1, 10, 100, 13, 23, 37, 5, 56, 9]
+
+console.log(second.sort());
 
 let age = [20, 25, 30, 24, 50];
+console.log(age.sort((a, b) => {
+	if (a < b) {
+		return -1
+	}
+	else if (a > b) {
+		return 1
+	}
+}));
+const users = [
+	{
+		username: 'fazilkhan',
+		fullname: 'Fazil ',
+		password: "fazilkhan"
+	},
+	{
+		username: 'adilkhan',
+		fullname: 'Adil ',
+		password: "adilkhan"
+	},
+	{
+		username: 'rahul5678',
+		fullname: 'Rahul ',
+		password: "adilkhan"
+	}
+]
+console.log(users.sort((a, b) => {
+	if (a.username < b.username) {
+		return -1
+	}
+	else if (a.username > b.username) {
+		return 1
+	}
+}));
 
-age.filter((item => item < 30)).map((age) => console.log(age));
+// age.filter((item => item < 30)).map((age) => console.log(age));
 
-let initial = 0;
-let sumOfArr = age.reduce((acu, current) => acu + current, initial)
+// let initial = 0;
+// let sumOfArr = age.reduce((acu, current) => acu + current, initial)
 
-console.log(sumOfArr);
+// console.log(sumOfArr);
+
+// const items = { id: 2, age: '6', name: 'fazil' };
+// const obj = Object.keys(items);
+// console.log(obj)
